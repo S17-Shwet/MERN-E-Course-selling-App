1b98dd8186d2a1c3e568b7ea9c665c207e324eb3
S17-Shwet <nichalshweta77@gmail.com>
2025-08-06 10:27:57 +0530

    admin added

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-import {Admin }from "../models/admin.model.js";
+import Admin from "../models/admin.model.js";
 import bcrypt from "bcryptjs";
 import { z } from "zod";
 import jwt from "jsonwebtoken";
